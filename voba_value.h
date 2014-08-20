e96f72704c3c5286ca88c5b6929962df5e8420fa
wcy123 <wcy123@gmail.com>
2014-08-20 14:35:43 +0800

    add macro VOBA_THROW, and VOBA_DEF_ARG

--- voba_value.h
+++ voba_value.h
@@ -22,6 +22,23 @@ void    voba_throw_exception(voba_value_t v);
 // some useful macros
 #define VOBA_FUNC __attribute__((aligned(16)))
 
+#define VOBA_THROW(...) voba_throw_exception(voba_make_string(VOBA_STRCAT(__VA_ARGS__)))
+
+#define VOBA_DEF_ARG(name,args,n,type)                                  \
+if(voba_array_len(args) <= n) {                                         \
+    VOBA_THROW(VOBA_CONST_CHAR("unexpected number of argument: "),      \
+               voba_str_fmt_uint32_t(n+1,10),                             \
+               VOBA_CONST_CHAR(" expected, but given "),                 \
+               voba_str_fmt_uint32_t(voba_array_len(args),10));         \
+}                                                                       \
+voba_value_t name = voba_array_at(args,n);                              \
+if(!type(name)){                                                        \
+    VOBA_THROW(VOBA_CONST_CHAR("wrong type of argument #") ,            \
+               voba_str_fmt_uint32_t(n,10),                             \
+               VOBA_CONST_CHAR(": `" #type "` expected, but given value is 0x"), \
+               voba_str_fmt_uint64_t(name,10));                         \
+}
+
 
 /* Local Variables: */
 /* mode:c */
