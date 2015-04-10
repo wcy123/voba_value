8e9ebcca1bbff9896cda69bd32a1e36ae6852bda
wcy123 <wcy123@gmail.com>
2015-04-10 16:34:55 +0800

    generator does not support TCO.

--- imp/generator.h
+++ imp/generator.h
@@ -10,6 +10,7 @@ see <https://github.com/wcy123/cg> for more details.
 #define VOBA_GENERATOR(s) VOBA_USER_DATA_AS(cg_t *,s)
 /** @brief The class object for generators. */
 extern voba_value_t voba_cls_generator;
+typedef voba_value_t (*voba_generator_t)(voba_value_t self, voba_value_t args);
 /** @brief Create a generator
 
  @return a callable object which capture a function pointer \a f, \a self and \a args.
@@ -21,4 +22,4 @@ cg_yield could be invoked to suspend the generator.
 @todo explain generator in details
 
 */
-INLINE voba_value_t voba_make_generator(voba_func_t f, voba_value_t self, voba_value_t args);
+INLINE voba_value_t voba_make_generator(voba_generator_t f, voba_value_t self, voba_value_t args);
